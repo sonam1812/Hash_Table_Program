45b27215d227298db6c69392bd0ad8163ee921ca
sonam1812 <sonampal832@gmail.com>
2021-10-14 19:24:54 +0530

    Added: file

---
+++ src/main/java/com/HashTable/App.java
@@ -0,0 +1,13 @@
+package com.HashTable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to hash table program" );
+    }
+}
